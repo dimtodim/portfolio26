96ac2abf6d0144aedd285ab777d679b2545e7555
aleksandar milosavljevic <dimtodim@gmail.com>
2025-11-27 15:15:41 +0100

    Add files via upload

--- _components/Container.tsx
+++ _components/Container.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="px-1 lg:px-2 mx-auto max-w-[2564px] overflow-hidden">
+    <div className="px-1 lg:px-2 mx-auto max-w-[2560px] overflow-hidden">
       {children}
     </div>
   );
