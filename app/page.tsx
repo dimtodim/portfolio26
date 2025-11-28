b067312c87138912480aeeb6221e028ee084e443
aleksandar milosavljevic <dimtodim@gmail.com>
2025-11-28 15:24:11 +0100

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import LogoMain from "@/_components/LogoMain";
 import Nav from "@/_components/Nav";
 import DesignContest from "@/_components/DesignContest";
 import LostLogosGalerry from "@/_components/LostLogosGallery";
+import VisitCards from "@/_components/VisitCards";
 import Vjezba from "@/_components/Vjezba";
 import OldCpuPcFanScene from "@/_components/OldCpuPcFanScene";
 import VokmenScene from "@/_components/VokmenScene";
@@ -21,6 +22,7 @@ export default function Home() {
         <LogoMain />
         <DesignContest />
         <LostLogosGalerry />
+        <VisitCards />
         <Vjezba />
         <div className="h-[270px]"></div>
         <OldCpuPcFanScene />
