4a58aa676dd3617dac14cc546a1f5da30040cf60
aleksandar milosavljevic <dimtodim@gmail.com>
2025-11-28 15:03:49 +0100

    Add files via upload

---
+++ VisitCards.tsx
@@ -0,0 +1,35 @@
+import PhysiotherapistFront from "./svg/visit_cards/physiotherapist_front";
+import PhysiotherapistBack from "./svg/visit_cards/physiotherapist_back";
+import NamaskarFront from "./svg/visit_cards/namaskar_front";
+import NamaskarBack from "./svg/visit_cards/namaskar_back";
+import TexasFront from "./svg/visit_cards/texas_front";
+import MotoFront from "./svg/visit_cards/moto_front";
+import MotoBack from "./svg/visit_cards/moto_back";
+
+const visitCards = [
+  { component: PhysiotherapistFront, className: "physiotherapist_front" },
+  { component: PhysiotherapistBack, className: "physiotherapist_back" },
+  { component: NamaskarFront, className: "namaskar_front" },
+  { component: NamaskarBack, className: "namaskar_back" },
+  { component: MotoFront, className: "moto_front" },
+  { component: MotoBack, className: "moto_back" },
+
+  { component: TexasFront, className: "texas_front" },
+
+  // dodaj još
+];
+
+export default function VisitCards() {
+  return (
+    <div className="w-full rounded-sm border-2 border-(--border-muted) grid grid-cols-1 md:grid-cols-2 gap-1 lg:gap-2 p-1 lg:p-2">
+      {visitCards.map(({ component: VisitCardComponent, className }, index) => (
+        <div
+          key={index}
+          className={`visit_card ${className} hover:cursor-pointer`}
+        >
+          <VisitCardComponent className={`${className} w-full h-full`} />
+        </div>
+      ))}
+    </div>
+  );
+}
